4e2813adffcd2e76003e8bdf5c05e905f8b258aa
unbre4kab1e <moujahiddine@gmail.com>
2024-07-31 15:19:08 +0100

    Add files via upload

--- templates/bulk-update-page.php
+++ templates/bulk-update-page.php
@@ -1,7 +1,7 @@
 <div class="wrap_price_updater">
-    <h1>Individual Price Updater</h1>
+    <h1>Bulk Price Updater</h1>
     <form method="post" action="">
-        <?php wp_nonce_field('individual_price_update', 'individual_price_update_nonce'); ?>
+        <?php wp_nonce_field('bulk_price_update', 'bulk_price_update_nonce'); ?>
         <label for="price_change">Enter Price Change:</label>
         <input type="number" id="price_change" name="price_change" step="0.01" required>
         <label for="change_type">Change Type:</label>
@@ -20,22 +20,6 @@
             <option value="add">Add</option>
             <option value="reduce">Reduce</option>
         </select>
-        <label>Select Products:</label>
-        <div id="product-list">
-            <?php
-            if (!empty($products_data)) {
-                foreach ($products_data as $product) {
-                    echo '<div>';
-                    echo '<input type="checkbox" name="product_ids[]" value="' . esc_attr($product['id']) . '"> ';
-                    echo esc_html($product['id']) . ' - ' . esc_html($product['name']) . ' ';
-                    echo '(Regular Price: ' . esc_html($product['regular_price']) . ', Sale Price: ' . esc_html($product['sale_price']) . ')';
-                    echo '</div>';
-                }
-            } else {
-                echo 'No products found.';
-            }
-            ?>
-        </div>
-        <input type="submit" name="update_individual_prices" value="Update Prices" class="button button-primary">
+        <input type="submit" name="update_prices" value="Update Prices" class="button button-primary">
     </form>
 </div>
